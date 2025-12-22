af9724d4f4b95ae02fc6f2ea26bceee077c9d704
Petro Maksymovych <68553007+amplipy@users.noreply.github.com>
2025-12-22 10:29:42 -0500

    Rename package from 'pubplotlib' to 'tweakfig'

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pubplotlib',
+    name='tweakfig',
     version='0.1',
     author='Petro Maksymovych',
     author_email='pmax20@gmail.com',
